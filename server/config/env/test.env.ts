37cf51ce19b5bedd2260a801b502c72bb180dcc5
Ricardo Bomfim <ricardoub@gmail.com>
2017-11-28 19:31:33 -0200

    aula33 - criando modulo dconfig

---
+++ server/config/env/test.env.ts
@@ -0,0 +1,12 @@
+module.exports = {
+  env: 'development',
+  db: 'ts-api-test',
+  dialect: 'postgres',
+  username: 'postgres',
+  password: 'postgres',
+  host: 'localhost',
+  serverPort: 3000,
+  pgPort: 5432,
+  dbURL: 'postgres://postgres:postgres@localhost:5432/ts-api-test',
+  secret: 's3cr3t'
+}
