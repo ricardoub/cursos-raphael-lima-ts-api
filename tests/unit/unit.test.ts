ca36fc942c6106126983b1635a80b0590f116590
Ricardo Bomfim <ricardoub@gmail.com>
2017-12-04 16:46:38 -0200

    aula41 - skelleton de testes

---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,29 @@
+import { testDouble, expect } from './config/helpers';
+
+describe('Testes Unitários do Controller', () => {
+
+  describe('Método Create', () => {
+    it('Deve criar um novo Usuário', () => {
+
+    })
+  });
+
+  describe('Método Update', () => {
+    it('Deve atualizar um Usuário', () => {
+
+    })
+  });
+
+  describe('Método Get Users', () => {
+    it('Deve retornar uma lista com todos os Usuários', () => {
+
+    })
+  });
+
+  describe('Método Delete', () => {
+    it('Deve deletar um Usuário', () => {
+
+    })
+  });
+
+});
