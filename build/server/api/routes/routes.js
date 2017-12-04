48636e254f6685329e6436da075abf8a76193d23
Ricardo Bomfim <ricardoub@gmail.com>
2017-12-04 17:50:00 -0200

    aula45 - Implementando Métodos para Atender as Rotas

--- build/server/api/routes/routes.js
+++ build/server/api/routes/routes.js
@@ -1,7 +1,9 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var routes_1 = require("../../modules/User/routes");
 var Routes = (function () {
     function Routes(app) {
+        this.router = new routes_1.default();
         this.getRoutes(app);
     }
     Routes.prototype.getRoutes = function (app) {
@@ -10,7 +12,7 @@ var Routes = (function () {
         app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`));
         */
         app.route('/api/users/all').get(this.router.index);
-        app.route('/api/users/create').post(this.router.cretae);
+        app.route('/api/users/create').post(this.router.create);
         app.route('/api/users/:id').get(this.router.findOne);
         app.route('/api/users/:id/update').put(this.router.update);
         app.route('/api/users/:id/destroy').delete(this.router.destroy);
--- server/api/routes/routes.ts
+++ server/api/routes/routes.ts
@@ -1,8 +1,12 @@
 import { Application, Request, Response } from 'express';
+import UserRoutes from '../../modules/User/routes';
 
 class Routes {
 
+  private router: UserRoutes;
+
   constructor(app: Application) {
+    this.router = new UserRoutes();
     this.getRoutes(app);
   }
 
@@ -12,7 +16,7 @@ class Routes {
     app.route('/ola/:nome').get((req: Request, res: Response) => res.send(`Hello, ${req.params.nome}`));
     */
     app.route('/api/users/all').get(this.router.index);
-    app.route('/api/users/create').post(this.router.cretae);
+    app.route('/api/users/create').post(this.router.create);
     app.route('/api/users/:id').get(this.router.findOne);
     app.route('/api/users/:id/update').put(this.router.update);
     app.route('/api/users/:id/destroy').delete(this.router.destroy);
