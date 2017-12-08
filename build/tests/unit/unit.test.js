cd16fd394855d3e60855e336782a9215933293be
Ricardo Bomfim <ricardoub@gmail.com>
2017-12-08 20:09:00 -0200

    aula68 - Refatorando os Testes Unitários - Testando Método Service GETALL

--- build/tests/unit/unit.test.js
+++ build/tests/unit/unit.test.js
@@ -1,20 +1,47 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var helpers_1 = require("./config/helpers");
+var service_1 = require("../../server/modules/User/service");
 describe('Testes Unitários do Controller', function () {
-    describe('Método Create', function () {
-        it('Deve criar um novo Usuário', function () {
-        });
-    });
-    describe('Método Update', function () {
-        it('Deve atualizar um Usuário', function () {
-        });
-    });
+    // describe('Método Create', () => {
+    //   it('Deve criar um novo Usuário', () => {
+    //     const novoUsuario = {
+    //       id: 1,
+    //       name: 'Novo Usuario',
+    //       email: 'novousuario@email.com',
+    //       password: '1234'
+    //     };
+    //
+    //     const user = new User();
+    //
+    //     return user.create(novoUsuario)
+    //               .then(data => {
+    //                 expect(data.dataValues).to.have.all.keys(
+    //                   ['email','id','name','password','updatedAt','createdAt']
+    //                 )
+    //               });
+    //   });
+    // });
+    //
+    // describe('Método Update', () => {
+    //   it('Deve atualizar um Usuário', () => {
+    //
+    //   })
+    // });
+    //
     describe('Método Get Users', function () {
         it('Deve retornar uma lista com todos os Usuários', function () {
+            var user = new service_1.default();
+            return user.getAll().then(function (data) {
+                helpers_1.expect(data).to.be.an('array');
+                helpers_1.expect(data[0]).to.have.all.keys(['email', 'id', 'name', 'password']);
+            });
         });
     });
-    describe('Método Delete', function () {
-        it('Deve deletar um Usuário', function () {
-        });
-    });
+    //
+    // describe('Método Delete', () => {
+    //   it('Deve deletar um Usuário', () => {
+    //
+    //   })
+    // });
 });
--- tests/unit/unit.test.ts
+++ tests/unit/unit.test.ts
@@ -1,29 +1,50 @@
 import { testDouble, expect } from './config/helpers';
+import User from '../../server/modules/User/service';
 
 describe('Testes Unitários do Controller', () => {
 
-  describe('Método Create', () => {
-    it('Deve criar um novo Usuário', () => {
-
-    })
-  });
-
-  describe('Método Update', () => {
-    it('Deve atualizar um Usuário', () => {
-
-    })
-  });
-
+  // describe('Método Create', () => {
+  //   it('Deve criar um novo Usuário', () => {
+  //     const novoUsuario = {
+  //       id: 1,
+  //       name: 'Novo Usuario',
+  //       email: 'novousuario@email.com',
+  //       password: '1234'
+  //     };
+  //
+  //     const user = new User();
+  //
+  //     return user.create(novoUsuario)
+  //               .then(data => {
+  //                 expect(data.dataValues).to.have.all.keys(
+  //                   ['email','id','name','password','updatedAt','createdAt']
+  //                 )
+  //               });
+  //   });
+  // });
+  //
+  // describe('Método Update', () => {
+  //   it('Deve atualizar um Usuário', () => {
+  //
+  //   })
+  // });
+  //
   describe('Método Get Users', () => {
     it('Deve retornar uma lista com todos os Usuários', () => {
-
+      const user = new User();
+      return user.getAll().then(data => {
+        expect(data).to.be.an('array');
+        expect(data[0]).to.have.all.keys(
+          ['email','id','name','password']
+        )
+      })
     })
   });
-
-  describe('Método Delete', () => {
-    it('Deve deletar um Usuário', () => {
-
-    })
-  });
+  //
+  // describe('Método Delete', () => {
+  //   it('Deve deletar um Usuário', () => {
+  //
+  //   })
+  // });
 
 });
